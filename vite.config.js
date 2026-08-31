c0c941d7ed6235204e040a11d4c7336dbec1c1b1
taktakahmad01 <103115512+taktakahmad01@users.noreply.github.com>
2026-08-31 16:19:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/mini-video-editor/"
+  base: "/mini-video-editor/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        editor: resolve(__dirname, "editor.html"),
+        loadTest: resolve(__dirname, "load-test.html")
+      }
+    }
+  }
 });
